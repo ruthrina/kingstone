864d38aaf83828dc74cd6fb5072bdb3e38134bd5
ruthrina <nassiwaruth2020@gmail.com>
2025-02-08 07:19:49 +0300

    change5

--- schoolproject/settings.py
+++ schoolproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a7qyi9!2*%8et_dknqo#w*!k*!=ah6$hlx=boontg9n&*3n6%k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
